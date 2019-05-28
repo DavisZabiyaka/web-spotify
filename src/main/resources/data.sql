930529e6bcce8fcdb07f62a23fe9723ff6a02f1b
Davis Zabiyaka <dzabiyaka1@gmail.com>
2019-05-28 01:09:31 -0400

    Add application properties and data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+TRUNCATE TABLE playlist;
+
+INSERT INTO playlist VALUES
+(1, 'Great', 100, 'ItsLit'),
+(2, 'SomethingAmazing', 1000, 'GoodPlaylist'),
+(3, 'Atandwa', 23, 'ThePrince'),
+(4, 'Probably', 10330, 'CouldBe'),
+(5, 'Cool', 1000, 'WasItMeantToBe?'),
+(6, 'GoodBoi', 23, 'SQL'),
+(7, 'InjectionofSQL', 8200, 'Strawberry'),
+(8, 'Shiiiiii....', 10, 'OK'),
+(9, 'Crashing', 22303, 'DownSide'),
+(10, 'WillItWork', 100, 'Hoe'),
+(11, 'HopeSO', 1000000, 'WhoKnows'),
+(12, 'MaybeNotDo', 2, 'STFU'),
+(13, 'Lucky', 927, 'Pilgrim'),
+(14, 'CanYouSmell', 9000, 'WhatDidISay'),
+(15, '15SeemsReasonable', 15, 'ThePrince');
+
+TRUNCATE TABLE playlist_seq;
+INSERT INTO playlist_seq VALUES(16);
