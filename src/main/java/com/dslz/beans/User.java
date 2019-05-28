80e3dffad6c90605e01792f6efe074382dc56910
Davis Zabiyaka <dzabiyaka1@gmail.com>
2019-05-28 17:08:33 -0400

    Add User repository and services

--- src/main/java/com/dslz/beans/User.java
+++ src/main/java/com/dslz/beans/User.java
@@ -17,8 +17,8 @@ public class User {
     @SequenceGenerator(name = "USER_SEQ", sequenceName = "USER_SEQ", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQ")
     private Integer userId;
-    @Column(name = "USERNAME")
-    private String username;
+    @Column(name = "USER_EMAIL")
+    private String userEmail;
     @Column(name = "PASSWORD", columnDefinition = "BLOB")
     private byte[] encryptedPassword;
     @Column(name = "SALT", columnDefinition = "BLOB")
@@ -28,17 +28,17 @@ public class User {
         super();
     }
 
-    public User(String username, byte[] encryptedPassword) {
-        this.username = username;
+    public User(String userEmail, byte[] encryptedPassword) {
+        this.userEmail = userEmail;
         this.encryptedPassword = encryptedPassword;
     }
 
     /**
      * 
-     * @return username of the user
+     * @return email of the user
      */
-    public String getUsername() {
-        return username;
+    public String getUserEmail() {
+        return userEmail;
     }
 
     /**
@@ -62,7 +62,7 @@ public class User {
         final int prime = 31;
         int result = 1;
         result = prime * result + Arrays.hashCode(encryptedPassword);
-        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((userEmail == null) ? 0 : userEmail.hashCode());
         return result;
     }
 
@@ -77,17 +77,17 @@ public class User {
         User other = (User) obj;
         if (!Arrays.equals(encryptedPassword, other.encryptedPassword))
             return false;
-        if (username == null) {
-            if (other.username != null)
+        if (userEmail == null) {
+            if (other.userEmail != null)
                 return false;
-        } else if (!username.equals(other.username))
+        } else if (!userEmail.equals(other.userEmail))
             return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "User [encryptedPassword=" + Arrays.toString(encryptedPassword) + ", username=" + username + "]";
+        return "User [encryptedPassword=" + Arrays.toString(encryptedPassword) + ", userEmail=" + userEmail + "]";
     }
 
 }
---
+++ src/main/java/com/dslz/services/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.dslz.services;
+
+import java.util.List;
+
+import com.dslz.beans.User;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<User> findAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User findUserByEmail(String userEmail) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(User user) {
+
+    }
+
+    @Override
+    public void createUser(User user) {
+
+    }
+    
+}
