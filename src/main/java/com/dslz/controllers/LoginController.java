66856930d873d3262d5cbe03dc5843869f4f654b
Davis Zabiyaka <dzabiyaka1@gmail.com>
2019-05-29 13:20:48 -0400

    Add authentication logic

--- src/main/java/com/dslz/controllers/LoginController.java
+++ src/main/java/com/dslz/controllers/LoginController.java
@@ -32,8 +32,9 @@ public class LoginController {
     @RequestMapping(value = "login" /* , method = RequestMethod.POST */)
     public String goToHomePageAfterSuccessfulLogin(@RequestParam("username") String username,
             @RequestParam("password") String password) {
-        System.out.printf("\nUsername: %s\tPassword: %s\n", username, password);
-        return "redirect:/partials/placeholder.html";
+        // System.out.printf("\nUsername: %s\tPassword: %s\n", username, password);
+        // return "redirect:/partials/placeholder.html";
+        return "Great!";
     }
 
     @RequestMapping(value = "users", method = RequestMethod.GET)
---
+++ src/main/java/com/dslz/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.dslz.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("abc@gmail.com").password("admin123").roles("ADMIN").build());
+        return manager;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+            // .loginPage("/#/login")
+            .permitAll();
+    }
+
+}
