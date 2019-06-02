23d549b1c3e0a9ba99328dc394e59f0b25ab4fdf
Davis Zabiyaka <dzabiyaka1@gmail.com>
2019-06-02 12:41:00 -0400

    Add caching back, need to figure out how to delete from cache

--- src/main/java/com/dslz/controllers/PlaylistController.java
+++ src/main/java/com/dslz/controllers/PlaylistController.java
@@ -1,11 +1,13 @@
 package com.dslz.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import com.dslz.beans.Playlist;
 import com.dslz.services.PlaylistService;
+import com.google.gson.Gson;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,20 +27,49 @@ public class PlaylistController {
     @Autowired
     PlaylistService playlistService;
     
-
+    /**
+     * API method that retrieves all playlists in the database
+     * @return an HTTP ResponseEntity
+     */
     @RequestMapping(value = "/playlists", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<Playlist>> getAllPlaylists() {
         List<Playlist> playlists = (List<Playlist>) playlistService.findAllPlaylists();
+        Gson g = new Gson();
+        List<String> jsonPlaylists = new ArrayList<>();
         if (playlists.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-        for (Playlist playlist : playlists) {
-            System.out.println(playlist);
+        /*
+         * Attempting to prefix JSON in order to prevent from JSON vulnerability.
+         * Angularjs automatically strips prefixes before processing it into JSON
+        */
+        for (int i = 0; i < playlists.size(); i++) {
+            jsonPlaylists.add(")]}',\n" + g.toJson(playlists.get(i)));
         }
+        for (String jsonPlaylist : jsonPlaylists) {
+            System.out.println(jsonPlaylist);
+        }
+        // List<Playlist> playlistsParsed = new ArrayList<>();
+        // for (int i = 0; i < jsonPlaylists.size(); i++) {
+        //     String j = jsonPlaylists.get(i);
+        //     System.out.println("J: " + j);
+        //     Playlist pl = g.fromJson(j, Playlist.class);
+        //     System.out.println("pl:" + pl);
+        //     //playlistsParsed.add(g.fromJson(jsonPlaylists.get(i), Playlist.class));
+        //     //System.out.println(playlistsParsed.get(i));
+        // }
+        String wtf = g.toJson(new Playlist("God", "tank", 0, null));
+        System.out.println(wtf);
+        System.out.println(new Gson().fromJson(wtf, Playlist.class));
         return new ResponseEntity<>(playlists, HttpStatus.OK);
     }
 
+    /**
+     * API that deletes a designated playlist based off of the playlistId
+     * @param playlistId - the id of the playlist to be deleted
+     * @return an HTTP ResponseEntity
+     */
     @RequestMapping(value = "/playlists/{playlistId}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> deletePlaylistById(@PathVariable("playlistId") Integer playlistId) {
         Playlist playlist = playlistService.findPlaylistById(playlistId.intValue());
@@ -52,6 +83,11 @@ public class PlaylistController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * API method that creates a new playlist
+     * @param playlist - the playlist which is created
+     * @return
+     */
     @RequestMapping(value = "/playlists/create", method = RequestMethod.POST)
     public ResponseEntity<Void> createPlaylist(@Valid @RequestBody Playlist playlist) {
         playlistService.createPlaylist(playlist);
